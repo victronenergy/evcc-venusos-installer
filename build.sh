f175aaf69c8b414876367839228a88c4ef8791b8
Philipp Trenz <mail@philipptrenz.de>
2024-01-19 12:07:13 +0100

    replacing wget with curl for improved os compatibility

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ version=$( cat evcc/version | tr -d " \t\n\r" )
 url="https://github.com/evcc-io/evcc/releases/download/${version}/evcc_${version}_linux-armv6.tar.gz"
 
 printf "loading and unpacking evcc version $version ... "
-wget -qO- "$url" | tar xz -C evcc
+curl --silent --output - -L "$url" | tar xz -C evcc
 echo "done"
 
 rm evcc/evcc.dist.yaml
